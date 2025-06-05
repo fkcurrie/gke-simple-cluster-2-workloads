47b78b0950495b577e3de16d84aabf0e2f060776
Frank Currie <fkcurrie@github.com>
2025-06-05 13:51:18 -0400

    Fix: Move terraform block to variables.tf and add required version

--- variables.tf
+++ variables.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+  }
+}
+
 variable "project_id" {
   description = "The project ID to deploy to"
   type        = string
