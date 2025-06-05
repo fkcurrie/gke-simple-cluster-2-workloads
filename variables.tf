67a6fceaa52c943cff158f9d797221c03fb7b0fd
Frank Currie <fkcurrie@github.com>
2025-06-05 13:53:18 -0400

    Fix: Move terraform block to main.tf following best practices

--- variables.tf
+++ variables.tf
@@ -1,13 +1,3 @@
-terraform {
-  required_version = ">= 1.0.0"
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 4.0"
-    }
-  }
-}
-
 variable "project_id" {
   description = "The project ID to deploy to"
   type        = string
